6e8969ee229a25c66d2a44c9546fe61c3f1217f6
NewAlexey <alexey.krupenia@gmail.com>
2021-12-27 18:59:25 +0300

    add prod build for gh-pages

--- src/helpers/AppContext.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { IContext } from '../interfaces/interfaces';
-
-export const contextData = {
-  data: 'context mat ego',
-};
-
-export const AppContext =
-  React.createContext<IContext>({});
